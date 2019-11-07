60a7321cc329f7f6d2409fef5e10a87f808d4ad9
BillyFarosa <57003140+BillyFarosa@users.noreply.github.com>
2019-11-07 09:37:04 +0700

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,1 +1,10 @@
-# labpy03
+n=100000000
+sum=0
+y=0
+laba = [int(0), int(0), int(n) * .1, int (n) * .1, int(n) * .5, int(n) * .5, int(n) * .5, int(n) * .2]
+print("Modal awal seorang pengusaha:", n)
+for i in laba :
+    sum = sum+i
+    y+=1
+    print("Laba bulan ke-",y,"sebesar:", i)
+print("Total laba adalah:", sum)
