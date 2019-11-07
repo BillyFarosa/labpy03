ff67d7776a830e22939efd2984fc22d89cb47d65
BillyFarosa <57003140+BillyFarosa@users.noreply.github.com>
2019-11-07 09:12:26 +0700

    Delete latihan1.py

--- latihan1.py
+++
@@ -1,6 +0,0 @@
-              Menampilkan n bilangan acak lebih kecil dari 0.5
- ![126](https://user-images.githubusercontent.com/57003140/68353924-986af280-013d-11ea-9b28-db495c702e05.png)    
-        
-              
-              Menggunakan From random Import random
-![125](https://user-images.githubusercontent.com/57003140/68354013-d49e5300-013d-11ea-8aed-e21c2710da7e.png)
